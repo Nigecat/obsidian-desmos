f563a19ee6b59a45833b25fda89d34c2a5e4ce41
Nigecat <nigel@nigelq.com>
2021-06-13 18:55:05 +1000

    Fix symlinks

--- test/.obsidian/plugins/obsidian-desmos/main.js
+++ test/.obsidian/plugins/obsidian-desmos/main.js
@@ -1,12 +1,1 @@
-'use strict';
-
-var obsidian = require('obsidian');
-
-class ObsidianDesmos extends obsidian.Plugin {
-    onload() {
-        console.log("Hello, World!");
-    }
-}
-
-module.exports = ObsidianDesmos;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOlsibWFpbi50cyJdLCJzb3VyY2VzQ29udGVudCI6bnVsbCwibmFtZXMiOlsiUGx1Z2luIl0sIm1hcHBpbmdzIjoiOzs7O01BRXFCLGNBQWUsU0FBUUEsZUFBTTtJQUM5QyxNQUFNO1FBQ0YsT0FBTyxDQUFDLEdBQUcsQ0FBQyxlQUFlLENBQUMsQ0FBQztLQUNoQzs7Ozs7In0=
+../../../../main.js
