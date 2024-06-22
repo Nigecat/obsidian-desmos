616ef2f14d0df3b17986935c881c4421987df419
Nigecat <development@nigelq.com>
2024-06-22 14:08:36 +1000

    Update desmos to v1.9 and rewrite render test system

--- src/settings.ts
+++ src/settings.ts
@@ -9,8 +9,6 @@ export enum CacheLocation {
 export interface Settings {
     /** The program version these settings were created in */
     version: string;
-    /** Whether to use an older version of the Desmos API (v1.5), this is required for offline mode to work */
-    use_legacy_desmos_api: boolean;
     // /** The debounce timer (in ms) */
     // debounce: number;
     cache: CacheSettings;
@@ -24,7 +22,6 @@ export interface CacheSettings {
 
 const DEFAULT_SETTINGS_STATIC: Omit<Settings, "version"> = {
     // debounce: 500,
-    use_legacy_desmos_api: false,
     cache: {
         enabled: true,
         location: CacheLocation.Memory,
@@ -42,9 +39,9 @@ export function DEFAULT_SETTINGS(plugin: Desmos): Settings {
 /** Attempt to migrate the given settings object to the current structure */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function migrateSettings(plugin: Desmos, settings: any): Settings {
-    if (!Object.prototype.hasOwnProperty.call(settings, "use_legacy_desmos_api")) {
-        settings.use_legacy_desmos_api = DEFAULT_SETTINGS_STATIC.use_legacy_desmos_api;
-    }
+    // if (!Object.prototype.hasOwnProperty.call(settings, "use_legacy_desmos_api")) {
+    //     settings.use_legacy_desmos_api = DEFAULT_SETTINGS_STATIC.use_legacy_desmos_api;
+    // }
 
     return settings as Settings;
 }
@@ -121,20 +118,5 @@ export class SettingsTab extends PluginSettingTab {
                     });
             }
         }
-
-        new Setting(containerEl)
-            .setName("Use Legacy Desmos API")
-            .setDesc(
-                "Whether to use a legacy version (v1.5) of the Desmos API over the current latest version, this must be enabled for offline usage to work as versions >1.5 require an internet connection. Enabling this will prevent any features from later versions from functioning correctly."
-            )
-            .addToggle((toggle) =>
-                toggle.setValue(this.plugin.settings.use_legacy_desmos_api).onChange(async (value) => {
-                    this.plugin.settings.use_legacy_desmos_api = value;
-                    await this.plugin.saveSettings();
-
-                    // Reset the display so the new state can render
-                    this.display();
-                })
-            );
     }
 }
---
+++ test/dev.md
@@ -0,0 +1,3 @@
+```desmos-graph
+(\frac{11-\sqrt{29}}{2},1)|label
+```
